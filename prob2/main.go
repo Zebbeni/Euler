5e26691e557a12b390917b4691025abb53c51a4d
Zebbeni <andrew.albers@gmail.com>
2016-07-12 16:52:04 -0600

    removed more unneeded code

--- prob2/main.go
+++ prob2/main.go
@@ -5,12 +5,10 @@ import "fmt"
 func main() {
 	var1 := 1
 	var2 := 2
-	new := 0
-	total := 0
+	new, total := 0, 0
 	n := 4000000
 	for var1 <= n {
 		if var1%2 == 0 {
-			fmt.Print("adding\n")
 			total += var1
 		}
 		new = var1 + var2
